168f8570b3b7683985a410b6c624a0e3ff3385fc
Marius Vollmer <marius.vollmer@nokia.com>
2009-08-18 19:01:38 +0300

    Also remove Makefiles in autoclean.sh.

--- autoclean.sh
+++ autoclean.sh
@@ -5,4 +5,5 @@
 rm -f aclocal.m4 config.guess config.sub configure depcomp install-sh missing
 rm -rf autom4te.cache
 rm -f ltmain.sh m4/libtool.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4 m4/lt~obsolete.m4
+rm -f `find . -name Makefile`
 rm -f `find . -name Makefile.in`
