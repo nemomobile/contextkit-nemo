2dd84c0630666dd3f996c8f5705299b4f5155f1b
Jean-Luc Lamadon <jean-luc.lamadon@nokia.com>
2009-10-01 19:46:35 +0300

    "test-bluez-plugin.py: added cltool support remove common directory under customer test move tests.xml under libcontextsubscriberbluez-tests for CI support

--- libcontextsubscriber-plugins/customer-tests/bluez-plugin/test-bluez-plugin.py
+++ libcontextsubscriber-plugins/customer-tests/bluez-plugin/test-bluez-plugin.py
@@ -21,7 +21,6 @@
 ## 02110-1301 USA
 ##
 ##
-## Requires python2.5-gobject and python2.5-dbus
 ##
 import sys
 import unittest
@@ -31,79 +30,68 @@ from subprocess import Popen, PIPE
 import time
 import signal
 from time import sleep
-
-def proc_kill(pid):
-        os.system('../common/rec-kill.sh %d' % pid)
+from ContextKit.cltool import CLTool
 
 def timeoutHandler(signum, frame):
-        raise Exception('Tests have been running for too long')
-
-def stdoutRead (object,lines):
-        list = []
-        for i in range(lines):
-                list.append(object.stdout.readline().rstrip())
-        return list
+    raise Exception('Tests have been running for too long')
 
 def set_bluez_property(property, value):
-        os.system('dbus-send --system --dest=org.bluez --print-reply --type=method_call /org/bluez/`pidof bluetoothd`/hci0 org.bluez.Adapter.SetProperty string:%s variant:boolean:%s' % (property, value))
-        time.sleep(1)
+    os.system("dbus-send --system --dest=org.bluez  --print-reply --type=method_call /org/bluez/`pidof bluetoothd`/hci0 org.bluez.Adapter.SetProperty string:%s variant:boolean:%s" % (property, value))
+    time.sleep(1)
 
 
 class BluezPlugin(unittest.TestCase):
 
-        def setUp(self):
-                os.environ["CONTEXT_PROVIDERS"] = "."
-                # FIXME: this won't work in the test package...
+    def setUp(self):
+        os.environ["CONTEXT_PROVIDERS"] = "."
+        # Make Bluetooth invisible and un-enabled
+        # Note: This test will alter the bluetooth settings of the system!
 
-                # Make Bluetooth invisible and un-enabled
-                # Note: This test will alter the bluetooth settings of the system!
+        set_bluez_property("Discoverable", "false")
+        set_bluez_property("Powered", "false")
 
-                set_bluez_property("Discoverable", "false")
-                set_bluez_property("Powered", "false")
+        self.context_client = CLTool("context-listen", "Bluetooth.Enabled"
+                                     ,"Bluetooth.Visible")
 
-                self.context_client = Popen(["context-listen","Bluetooth.Enabled","Bluetooth.Visible"],stdin=PIPE,stdout=PIPE,stderr=PIPE)
-                self.initial_output = stdoutRead(self.context_client, 2)
-                self.initial_output.sort()
+    def tearDown(self):
 
-        def tearDown(self):
+        # Restore some default values for Bluez
+        set_bluez_property("Discoverable", "false")
+        set_bluez_property("Powered", "true")
 
-                # Restore some default values for Bluez
-                set_bluez_property("Powered", "true")
-                set_bluez_property("Discoverable", "false")
+    def testInitial(self):
 
-                proc_kill(self.context_client.pid)
+        self.assert_(self.context_client.expect(CLTool.STDOUT, "Bluetooth.Visible = bool:false\nBluetooth.Enabled = bool:false"
+                        , 10)) # timeout == 1 second
 
-        def testInitial(self):
-                self.assertEqual(self.initial_output[0].strip(), "Bluetooth.Enabled = bool:false")
-                self.assertEqual(self.initial_output[1].strip(), "Bluetooth.Visible = bool:false")
 
-        def testEnabledAndVisible(self):
-                # Enable
-                set_bluez_property("Powered", "true")
-                actual = stdoutRead(self.context_client,1)
-                expected = ["Bluetooth.Enabled = bool:true"]
-                self.assertEqual(actual, expected)
+    def testEnabledAndVisible(self):
+        # Enable
+        set_bluez_property("Powered", "true")
+        self.assert_(self.context_client.expect(CLTool.STDOUT,
+                              "Bluetooth.Visible = bool:false\nBluetooth.Enabled = bool:true",
+                              3)) # timeout == 1 second
 
-                # Set visible
-                set_bluez_property("Discoverable", "true")
-                actual = stdoutRead(self.context_client,1)
-                expected = ["Bluetooth.Visible = bool:true"]
-                self.assertEqual(actual, expected)
+        # Set visible
+        set_bluez_property("Discoverable", "true")
+        self.assert_(self.context_client.expect(CLTool.STDOUT,
+                              "Bluetooth.Visible = bool:true",
+                              3)) # timeout == 1 second
 
-                # Set invisible
-                set_bluez_property("Discoverable", "false")
-                actual = stdoutRead(self.context_client,1)
-                expected = ["Bluetooth.Visible = bool:false"]
-                self.assertEqual(actual, expected)
+        # Set invisible
+        set_bluez_property("Discoverable", "false")
+        self.assert_(self.context_client.expect(CLTool.STDOUT,
+                              "Bluetooth.Visible = bool:false",
+                              3)) # timeout == 1 second
 
-                # Disable
-                set_bluez_property("Powered", "false")
-                actual = stdoutRead(self.context_client,1)
-                expected = ["Bluetooth.Enabled = bool:false"]
-                self.assertEqual(actual, expected)
+        # Disable
+        set_bluez_property("Powered", "false")
+        self.assert_(self.context_client.expect(CLTool.STDOUT,
+                              "Bluetooth.Enabled = bool:false",
+                              3)) # timeout == 1 second
 
 if __name__ == "__main__":
-        sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 1)
-        signal.signal(signal.SIGALRM, timeoutHandler)
-        signal.alarm(30)
-        unittest.main()
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 1)
+    signal.signal(signal.SIGALRM, timeoutHandler)
+    signal.alarm(30)
+    unittest.main()
