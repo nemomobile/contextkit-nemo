6114cd99e08f86504d659c128a45d4dc77cd7712
Jean-Luc Lamadon <jean-luc.lamadon@nokia.com>
2009-11-12 20:25:40 +0200

    test-bluez-plugin.py: if hci0 is down, set it up

--- libcontextsubscriber-plugins/customer-tests/bluez-plugin/test-bluez-plugin.py
+++ libcontextsubscriber-plugins/customer-tests/bluez-plugin/test-bluez-plugin.py
@@ -46,15 +46,10 @@ class BluezPlugin(unittest.TestCase):
         os.environ["CONTEXT_PROVIDERS"] = "."
         # Make Bluetooth invisible and un-enabled
         # Note: This test will alter the bluetooth settings of the system!
-        os.system("hciconfig -a")
-        os.system("ps -Af | grep -i bluetooth")
         os.system("stop bluetoothd")
         os.system("start bluetoothd &")
         os.system("dbusnamewatcher --system org.bluez 10")
-        os.system("qdbus --literal --system org.bluez / org.bluez.Manager.DefaultAdapter")
-        os.system("hciconfig -a")
-        os.system("ps -Af | grep -i bluetooth")
-        print "Restart is over"
+        os.system("hciconfig hci0 up")
         set_bluez_property("Discoverable", "false")
         set_bluez_property("Powered", "false")
 
