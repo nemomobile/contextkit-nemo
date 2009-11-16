ec19c713fb51e39fd091d1cafba6caa1590208e1
Marja Hassinen <ext-marja.2.hassinen@nokia.com>
2009-11-16 13:43:12 +0200

    lcs bluez plugin: Coverage also for the bluez plugin cust tests.

--- libcontextsubscriber-plugins/customer-tests/bluez/runTests.sh
+++ libcontextsubscriber-plugins/customer-tests/bluez/runTests.sh
@@ -2,5 +2,27 @@
 
 which context-listen 1>/dev/null || { echo "Install contextkit-utils to run the test" && exit 1 ; }
 
-CONTEXT_SUBSCRIBER_PLUGINS=../../bluez/.libs/ python2.5 test-bluez-plugin.py
+# Ensure that the plugin is built and that the test program finds it
+if [ -n "$COVERAGE" ]
+then
+    make -C coverage-build
+    export CONTEXT_SUBSCRIBER_PLUGINS=coverage-build/.libs
+    rm -rf coverage-build/.libs/*.gcda
+else
+    export CONTEXT_SUBSCRIBER_PLUGINS=../../bluez/.libs
+    make -C ../fullscreen
+fi
+
+# Run the tests
+python2.5 test-bluez-plugin.py
+
+if [ -n "$COVERAGE" ]
+then
+    echo "computing coverage"
+    rm -rf coverage
+    mkdir -p coverage
+    lcov --directory coverage-build/.libs/ --capture --output-file coverage/all.cov
+    lcov --extract coverage/all.cov '*/bluez/*.cpp' --output-file coverage/bluez.cov
+    genhtml -o coverage/ coverage/bluez.cov
+fi
 
