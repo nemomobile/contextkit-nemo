1315f7dcebec06ec884c4913cd5c1e412d5182c5
Jani Lehmuskoski <ext-jani.i.lehmuskoski@nokia.com>
2010-02-10 16:58:26 +0200

    event polling

--- libcontextsubscriber-plugins/battery/sandbox/main.cpp
+++ libcontextsubscriber-plugins/battery/sandbox/main.cpp
@@ -5,70 +5,120 @@ extern "C" {
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/inotify.h>
+#include <sys/ioctl.h>
+#include <QCoreApplication>
+#include <QSocketNotifier>
+#include <QFile>
+#include <batterywatcher.h>
+#include <QFileSystemWatcher>
+#include <QTimer>
+#include <QDebug>
+#include <QVarLengthArray>
 
-static int32_t	_sd = -1;
+#define BMEIPC_EVENT	"/tmp/.bmeevt"
 
-
-
-int main(int argc, char *argv[])
+BatteryWatcher::BatteryWatcher()
 {
+    bmeipc_epush(0);
+
+/*  Alternative version
+    QFileSystemWatcher *fsw = new QFileSystemWatcher(this);
+    fsw->addPath(QString(BMEIPC_EVENT));
+    connect(fsw, SIGNAL(fileChanged(QString)), SLOT(readFromInotify()));
+*/
+
+    inotifyFd = bmeipc_eopen(-1); // Mask is thrown away
+    fcntl(inotifyFd, F_SETFD, FD_CLOEXEC);
+    int wd = inotify_add_watch(inotifyFd, BMEIPC_EVENT, (IN_CLOSE_WRITE | IN_DELETE_SELF));
+    if (0 > wd) {
+        qDebug() << "inotify_add_watch failed";
+        exit(-1);
+    }
+    QSocketNotifier *sn = new QSocketNotifier(inotifyFd, QSocketNotifier::Read, this);
+    connect(sn, SIGNAL(activated(int)), this, SLOT(readFromInotify()));
+}
+
+void pullStats()
+{
+    int32_t _sd = -1;
     bmestat_t st;
 
-    if (0 > (_sd = bmeipc_open())){
-        fprintf(stderr, "Failed to connect BME server, %s\n",
-                strerror(errno));
+    if (0 > (_sd = bmeipc_open())) {
+        qDebug() << "Failed to connect BME server, " <<  strerror(errno);
         exit(errno);
     }
-    printf("Connected to BME server\n");
 
     if (0 > bmeipc_stat(_sd, &st)) {
-        fprintf(stderr, "Failed to get statistics, %s\n",
-                strerror(errno));
+        qDebug() << "Failed to get statistics, " << strerror(errno);
     }
 
     switch (st[CHARGING_STATE]) {
     case CHARGING_STATE_STOPPED:
         if (BATTERY_STATE_FULL != st[BATTERY_STATE]) {
-            fprintf(stdout,"off\n");
+            qDebug() << "off";
         } else {
-            fprintf(stdout,"full\n");
+            qDebug() << "full";
         }
         break;
 
     case CHARGING_STATE_STARTED:
-        fprintf(stdout,"chaging started\n");
+      qDebug() << "Charging state:" << "charging started";
         break;
 
     case CHARGING_STATE_SPECIAL:
-        fprintf(stdout,"special\n");
+        qDebug() << "Charging state:" << "special";
         break;
     case CHARGING_STATE_ERROR:
-        fprintf(stdout,"error\n");
+        qDebug() << "Charging state:" << "error";
         break;
     default:
-        fprintf(stdout,"unknown\n");
+        qDebug() << "Charging state:" << "unknown";
 
     }
 
     switch (st[BATTERY_STATE]) {
     case BATTERY_STATE_EMPTY:
-        fprintf(stdout,"empty\n");
+        qDebug() << "Battery state:" << "empty";
         break;
     case BATTERY_STATE_LOW:
-        fprintf(stdout,"low\n");
+        qDebug() << "Battery state:" << "low";
         break;
     case BATTERY_STATE_OK:
-        fprintf(stdout,"ok\n");
+        qDebug() << "Battery state:" << "ok";
         break;
     case BATTERY_STATE_FULL:
-        fprintf(stdout,"full\n");
+        qDebug() << "Battery state:" << "full";
         break;
     case BATTERY_STATE_ERROR:
     default:
-        fprintf(stdout,"unknown\n");
+        qDebug() << "Battery state:" << "unknown";
         break;
     }
 
-    printf("level:%d bars:%d\n",st[BATTERY_LEVEL_NOW],st[BATTERY_LEVEL_MAX]);
+    qDebug() << "level:" << st[BATTERY_LEVEL_NOW] << "bars:" << st[BATTERY_LEVEL_MAX] 
+	     << "capacity:" << st[BATTERY_CAPA_NOW] << "/" << st[BATTERY_CAPA_MAX];
+}
 
+void BatteryWatcher::readFromInotify()
+{
+    // Empty buffer. Should not blog, since we got signal from QSocketNotifier
+    int buffSize = 0;
+    ioctl(inotifyFd, FIONREAD, (char *) &buffSize);
+    QVarLengthArray<char, 4096> buffer(buffSize);
+    buffSize = read(inotifyFd, buffer.data(), buffSize);
+
+    pullStats();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    pullStats();
+
+    BatteryWatcher bt;
+    
+    return app.exec();
 }
